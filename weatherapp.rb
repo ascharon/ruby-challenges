920a6c91ca46447eb28b6eafe3bfa9af60dbf63c
Janelle Scharon <ascharon@yahoo.com>
2015-06-28 21:15:20 +0200

    Weather app final project

--- weatherapp.rb
+++ weatherapp.rb
@@ -6,3 +6,5 @@ client = Weatherman::Client.new
 weather = client.lookup_by_location(location).condition['temp']
 forcasting = client.lookup_by_location(location).description
 puts "The temperature is #{weather} Celcius and the current conditions #{forcasting}"
+
+#final project 
